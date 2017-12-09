11cc0cfcb9148fc171c4221601a6d0b89e7d9fb3
skyhrsevvv <34390367+skyhrsevvv@users.noreply.github.com>
2017-12-09 14:59:54 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,4 +139,4 @@ client.on('message', msg => {
 });
 
 
-client.login('MzY2NTk0MjgxOTk1MTA4MzYz.DQ0F4g.8eo7K_-P2y-Mu1oXMmGo
+client.login('MzY2NTk0MjgxOTk1MTA4MzYz.DQ0F4g.8eo7K_-P2y-Mu1oXMmGo3WaZPXc');
